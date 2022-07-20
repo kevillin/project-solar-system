76e44b28e50f43d708f8f1db7ff770f3565e1599
Kevillin Santos <kevillin_santos@hotmail.com>
2022-07-20 14:57:34 -0300

    iniciando o projeto Solar System

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+    <p>Sistema Solar</p>
+    <p>Esse é o começo</p>
+    );
+    
   }
 }
 
