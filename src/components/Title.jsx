59264160f46e64add5fe74e0654f1708a0c32d6b
Kevillin Santos <kevillin_santos@hotmail.com>
2022-07-20 16:51:56 -0300

    Requisito 4 - tentando mais uma vez resolver problemas

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Title extends Component {
   render() {
+    const { headline } = this.props
     return (
-      <h2>{this.props.headline}</h2>
+      <h2>{headline}</h2>
     );
   }
+}
+
+Title.propTypes = {
+  headline: PropTypes.string
 }
