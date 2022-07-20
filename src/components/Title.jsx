2bf89f5c95f4803fc7bf3b3a82afb187f7846815
Kevillin Santos <kevillin_santos@hotmail.com>
2022-07-20 16:26:15 -0300

    Requisito 3 e tentando resolver pendencia dos outros requisitos

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 
 export default class Title extends Component {
   render() {
-    const { headline } = this.props;
     return (
-      <h2>{ headline }</h2>
+      <h2>{this.props.headline}</h2>
     );
   }
 }
